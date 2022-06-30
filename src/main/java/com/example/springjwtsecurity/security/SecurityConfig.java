e63a942d2b9b82be902ec124474a4bbc1e012f66
mobin-newroz <ahmobin@newroztech.com>
2022-06-30 15:02:22 +0600

    feat : jwt token add and custom authorization filter

--- src/main/java/com/example/springjwtsecurity/security/SecurityConfig.java
+++ src/main/java/com/example/springjwtsecurity/security/SecurityConfig.java
@@ -1,17 +1,21 @@
 package com.example.springjwtsecurity.security;
 
 import com.example.springjwtsecurity.filter.CustomAuthenticationFilter;
+import com.example.springjwtsecurity.filter.CustomAuthorizationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -28,10 +32,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        //for custom login url::
+        CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(authenticationManagerBean());
+        customAuthenticationFilter.setFilterProcessesUrl("/api/login");
+
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-        http.authorizeRequests().anyRequest().permitAll();
-        http.addFilter(new CustomAuthenticationFilter(authenticationManagerBean()));
+
+        http.authorizeRequests().antMatchers("/api/login/**").permitAll();
+        http.authorizeRequests().antMatchers(HttpMethod.GET,"/api/users/").hasAnyAuthority("USER");
+        http.authorizeRequests().antMatchers(HttpMethod.GET,"/api/users/**").hasAnyAuthority("USER");
+        http.authorizeRequests().antMatchers(HttpMethod.POST,"/api/users/**").hasAnyAuthority("ADMIN");
+//        http.authorizeRequests().anyRequest().permitAll(); //this will permit all route without authentication
+        http.authorizeRequests().anyRequest().authenticated(); //this will permit all route by authentication
+        http.addFilter(customAuthenticationFilter);
+        http.addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 
     @Bean
